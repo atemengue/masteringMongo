7ee9ca9105448a99e94bb910f1f466f955ca960c
atemengue <moafembe@gmail.com>
2020-03-14 13:45:20 +0100

    created first model

--- test/test_helper.js
+++ test/test_helper.js
@@ -1,10 +1,11 @@
 /** @format */
 
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-mongoose.connect('mongodb://localhost/user_test');
+// another code for connect mongo db tutorial
+mongoose.connect("mongodb://localhost/user_test");
 mongoose.connection
-  .once('open', () => console.log('Goog to go!!'))
-  .on('error', error => {
-    console.warn('Warning', error);
+  .once("open", () => console.log("Goog to go!!"))
+  .on("error", error => {
+    console.warn("Warning", error);
   });
