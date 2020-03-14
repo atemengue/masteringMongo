e3a5f8637a9e7f1b3ad23830588a31d619a616aa
atemengue <moafembe@gmail.com>
2020-03-14 15:15:12 +0100

    first solid architecture for mocha testing

--- test/create.test.js
+++ test/create.test.js
@@ -1,8 +1,12 @@
 const assert = require("assert");
 const User = require("../src/models/user");
 describe("Creating records", () => {
-  it("saves a user", () => {
+  it("saves a user", done => {
     const joe = new User({ name: "Joe" });
-    joe.save();
+    joe.save().then(() => {
+      //has joe beem save successfully ?
+      assert(!joe.isNew);
+      done();
+    });
   });
 });
--- test/test_helper.js
+++ test/test_helper.js
@@ -2,13 +2,19 @@
 
 const mongoose = require("mongoose");
 
-// another code for connect mongo db tutorial
-mongoose.connect("mongodb://localhost/user_test", { useUnifiedTopology: true });
-mongoose.connection
-  .once("open", () => console.log("Goog to go!!"))
-  .on("error", error => {
-    console.warn("Warning", error);
+// Hook qui s'execute seulement un fois wrapper mongoConnection by mocha
+before(done => {
+  // another code for connect mongo db tutorial
+  mongoose.connect("mongodb://localhost/user_test", {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
   });
+  mongoose.connection
+    .once("open", () => done())
+    .on("error", error => {
+      console.warn("Warning", error);
+    });
+});
 
 // Hook only avialable on mocha juste pour des tests
 beforeEach(done => {
