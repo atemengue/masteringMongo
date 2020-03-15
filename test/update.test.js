bf45490e2946aa5bd0ed5a4a83b5d9daa0026fca
atemengue <moafembe@gmail.com>
2020-03-15 11:34:54 +0100

    using mongo update operator

--- test/update.test.js
+++ test/update.test.js
@@ -5,7 +5,7 @@ describe("Updating a user", () => {
   let joe;
 
   beforeEach(done => {
-    joe = new User({ name: "Joe" });
+    joe = new User({ name: "Joe", postCount: 0 });
     joe.save().then(_ => done());
   });
 
@@ -25,11 +25,27 @@ describe("Updating a user", () => {
   });
 
   it("A model instance can update", done => {
-    joe.update({ name: "Regis" }).then(async () => {
+    joe.updateOne({ name: "Regis" }).then(async () => {
       // updateOne deprecated
       const user = await User.findOne({ name: "Joe" });
       assert(user === null);
       done();
     });
+  });
+
+  it("A model class can update", () => {});
+
+  it("A model class can updare one record", () => {});
+
+  it("A model class can find and ", () => {});
+
+  it("A user can have their postcount incremented by 1", done => {
+    User.updateOne({ name: "Joe" }, { $inc: { postCount: 1 } })
+      .then(() => User.findOne({ name: "Joe" }))
+      .then(user => {
+        console.log(user);
+        assert(user.postCount === 1);
+        done();
+      });
   });
 });
